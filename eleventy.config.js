afa205d42b5394b602d9efbc618ed3db93f3aad8
Andrew Gurylev <andrew@gurylev.com>
2025-03-30 01:10:43 +0200

    feat(content): add links to launcher, guidelines, discord, telegram

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,16 +1,28 @@
+import { EleventyRenderPlugin } from "@11ty/eleventy";
+
 export default async function(eleventyConfig) {
   const plugins = [
     { packageName: '@11ty/eleventy-plugin-directory-output' },
-    { packageName: '@11ty/eleventy-navigation' }
+    { packageName: '@11ty/eleventy-navigation' },
   ]
   const assetsToCopy = [
     { 'assets/fonts': 'fonts' },
+    { 'assets/img': 'img' },
   ]
+  const importModule = async (name) => {
+    const { default: module } = await import(name)
+    return module
+  }
+
+	eleventyConfig.addPlugin(EleventyRenderPlugin);
+  eleventyConfig.amendLibrary("md", async (mdLib) => {
+    mdLib.use(await importModule("markdown-it-bracketed-spans"))
+    mdLib.use(await importModule("markdown-it-attrs"))
+  });
+
 
   for (const { packageName, options = {} } of plugins) {
-    const { default: importedPlugin } = await import(packageName)
-
-    eleventyConfig.addPlugin(importedPlugin, options)
+    eleventyConfig.addPlugin(await importModule(packageName), options)
   }
 
   const filters = await import("./_11ty/filters.js")
@@ -24,8 +36,4 @@ export default async function(eleventyConfig) {
   }
 
   eleventyConfig.addWatchTarget('assets/css/**/*.css');
-}
-
-export const config = {
-  htmlTemplateEngine: "njk",
 }
---
+++ index.md
@@ -0,0 +1,18 @@
+---
+layout: post.njk
+---
+
+Сайт-заглушка, находится в работе{style="display: block; margin: auto; width: fit-content;"}
+
+![logo](/img/logo.png){.logo} 
+
+[Скачать установщик лончера OdinSons](https://server.odinsons.fun/launcher/OdinsonsSetup.exe){download style="display: block; margin: auto; width: fit-content;"}
+
+[Правила сообщества OdinSons](https://docs.google.com/document/d/1WXLEXULYd0KLtrCduEXAUb57RPMkqpIFZPy3U_WH69M/edit?usp=sharing){target="_blank" style="display: block; margin: auto; width: fit-content;"}
+
+[Перейти в дискорд](https://discord.gg/eTteBxWcfu){style="display: block; margin: auto; width: fit-content;"}
+
+[Перейти в телеграм](https://t.me/+AHY_F2jClNJmNTcy){style="display: block; margin: auto; width: fit-content;"}
+(в тг только новости, весь движ в дискорде){style="display: block; margin: auto; width: fit-content;"}
+
+<iframe src="https://discord.com/widget?id=1276564994301104313&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts" style="display: block; margin: auto; width: fit-content; padding: 1em;></iframe>
